34b1ef95312e41c3551572a15a03f8555f617215
Eunsub Kim <eunsub@gmail.com>
2021-03-07 22:53:47 +0900

    ...

--- action.js
+++ action.js
@@ -5,17 +5,23 @@ const dayjs = require('dayjs');
 
 
 (async () => {
-  let codes = (await fs.readFile("./codes.lst")).toString().split(/\n/);
-  let ymd = dayjs().format('YYYYMMDD')
-  // console.log(codes)
+  try {
+    let codes = (await fs.readFile("./codes.lst")).toString().split(/\n/);
+    let ymd = dayjs().format('YYYYMMDD')
+    // console.log(codes)
 
-  let res = {}
-  for(var i in codes){
-    let cd = codes[i]
-    let url = `https://navercomp.wisereport.co.kr/company/ajax/c1050001_data.aspx?flag=2&cmp_cd=${cd}&finGubun=IFRSL&frq=0&sDT=${ymd}&chartType=svg`
-    let {data} = await axios(url)
-    res[cd] = data
+    let res = {}
+    for (var i in codes) {
+      let cd = codes[i]
+      let url = `https://navercomp.wisereport.co.kr/company/ajax/c1050001_data.aspx?flag=2&cmp_cd=${cd}&finGubun=IFRSL&frq=0&sDT=${ymd}&chartType=svg`
+      let {
+        data
+      } = await axios(url)
+      res[cd] = data
+    }
+
+    await fs.writeJSON(__dirname + "/d/" + ymd + ".json", res)
+  } catch (err) {
+    console.log("ERR", err)
   }
-
-  await fs.writeJSON(__dirname + "/d/" + ymd + ".json", res)
 })();
